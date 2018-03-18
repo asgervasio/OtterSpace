c38a486e87ecb8cabe1a4f641122fd502fae330b
Aaron Gervasio <aaron.gervasio@gmail.com>
2018-03-18 19:26:11 -0400

    Set up gamePage servlet and fixed issues in jsp, added server jar files.

--- OtterSpace/src/edu/ycp/cs320/otterspace/servlet/GameServlet.java
+++ OtterSpace/src/edu/ycp/cs320/otterspace/servlet/GameServlet.java
@@ -1,5 +1,23 @@
 package edu.ycp.cs320.otterspace.servlet;
 
-public class GameServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GameServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException 
+	{
+		
+		System.out.println("GamePage Servlet: doGet");
+		
+		req.getRequestDispatcher("/_view/GamePage.jsp").forward(req, resp);
+	}
 }
