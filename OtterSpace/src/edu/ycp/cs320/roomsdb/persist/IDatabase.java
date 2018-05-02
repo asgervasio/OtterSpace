91bca40db85e5ce639bb1ecd712b0a5332a53b50
Aaron Gervasio <aaron.gervasio@gmail.com>
2018-05-02 01:09:50 -0400

    PersistingSQL

--- OtterSpace/src/edu/ycp/cs320/roomsdb/persist/IDatabase.java
+++ OtterSpace/src/edu/ycp/cs320/roomsdb/persist/IDatabase.java
@@ -12,40 +12,33 @@ public interface IDatabase {
 
 	public Room findRoomUsingRoomId(int roomId);
 	
-	public Item insertItem(Item item);
-	public Item findItemUsingTitle(String title);
-	public Item findItemUsingItemId(int itemId);
-	public List<Item> findItemsUsingLocation(int location);
+	public Item insertItem(Item item, String username);
+	public Item findItemUsingTitle(String title, String username);
+	public Item findItemUsingItemId(int itemId, String username);
+	public List<Item> findItemsUsingLocation(int location, String username);
 	public Integer findRoomIdFromConnection(String connection);
 
-	public Player insertPlayer(Player player);
-	public Player findPlayerUsingName(String name);
-	public Player findPlayerUsingLocation(Room roomLoc);
+	public Player insertPlayer(Player actor, String username);
+	public Player findPlayerUsingName(String name, String username);
+	public Player findPlayerUsingLocation(Room roomLoc, String username);
   
 	//User-based methods
 	List<User> getAccountInfo(String Username);
-	List<User> addUserToDatabase(String Username, String pass, String email, String first, String last);
 	List<User> DeleteUserFromDatabase(String name, String pswd);
 	List<User> changePassword(String name, String pswd, String newPassword);
 	List<User> findAllUsers();
 	List<User> findUserByLastName(String lastname);
 	List<User> matchUsernameWithPassword(String Username, String pass);
-	String insertConsole(String data);
-	List<String> loadConsole();
-	String insertChange(String typeChange, int typeId, int healthChange, String locatTypeChange, int locatTypeId);
+	String insertConsole(String data, String username);
+	List<String> loadConsole(String username);
 
 
-		List<User> getAccountInfo(String Username);
-		Boolean    addUserToDatabase(String Username, String pass, String email, String first, String last);
-		List<User> DeleteUserFromDatabase(String name, String pswd);
-		String changeInfo(String newEmail, String username, String pswd, String newPassword);
-		List<User> findAllUsers();
-		List<User> findUserByLastName(String lastname);
-		List<User> matchUsernameWithPassword(String Username, String pass);
+	Boolean addUserToDatabase(String Username, String pass, String email, String first, String last);
+	String changeInfo(String newEmail, String username, String pswd, String newPassword);
+	public void createTables(String username);
+	public void createPersistingTables(String username);
     
-  //Console methods
-	  String insertConsole(String data);
-	  List<String> loadConsole();
+
 
 
 }
